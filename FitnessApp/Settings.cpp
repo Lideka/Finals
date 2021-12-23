2042c1a925acb7a9cb94e22aa8d28b3286ef3ec1
Lidekius <39699943+Lideka@users.noreply.github.com>
2021-12-23 12:46:33 +0200

    Now we can add elements to global list

--- FitnessApp/Settings.cpp
+++ FitnessApp/Settings.cpp
@@ -1,6 +1,7 @@
 #include "Settings.h"
 
 #include "Database.h"
+#include "GUIInterface.h"
 
 Settings::Settings(QObject *parent) : QObject(parent)
 {
@@ -9,16 +10,33 @@ Settings::Settings(QObject *parent) : QObject(parent)
 
 void Settings::addElement(QString Name, QString Calories, QString Description)
 {
+   //Check if exercise with the same name already exists
    std::string Table;
+   std::string Element;
 
    if(m_IsExerciseSelected)
+   {
       Table = "Exercises";
+      Element = "exercise";
+   }
    else
+   {
       Table = "Dishes";
+      Element = "dish";
+   }
 
-   QList<QVariantList> res = GlobalDatabase->ExecuteSelectQuerry(Table, "Id", "Name = " + Name.toStdString());
+   GlobalDatabase->Open();
+   QList<QVariantList> res = GlobalDatabase->ExecuteSelectQuerry(Table, "Id", "Name = \"" + Name.toStdString() + "\"");
 
-   //if(!res.empty())
+   if(!res.empty())
+   {
+      GlobalDatabase->Close();
+      GlobalGUI->ShowErrorPopup("Exercise with this name already exists!");
+      return;
+   }
 
+   assert(GlobalDatabase->ExecuteInsertQuerry(Table, "Name, Calories, Description", "\"" + Name.toStdString() + "\", \"" + Calories.toStdString() + "\", \"" + Description.toStdString() + "\""));
+   GlobalDatabase->Close();
 
+   GlobalGUI->ShowMessagePopup("Success!", "Selected " + Element + " added successfuly!");
 }
